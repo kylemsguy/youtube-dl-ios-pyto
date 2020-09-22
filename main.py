8a474937a683adcf999ef6e2a034364b33f68324
Kyle Zhou <kylezhou2002@hotmail.com>
2020-09-21 20:05:35 -0700

    Debug what arguments actually get passed

--- main.py
+++ main.py
@@ -5,16 +5,21 @@ import os
 from youtube_dl import YoutubeDL
 
 if len(sys.argv) != 2:
-	raise IndexError('usage: %s url' % (sys.argv[0]))
+	raise IndexError('usage: %s url;type' % (sys.argv[0]))
 url = sys.argv[1]
+#dl_type = sys.argv[2]
 print('url: %s' % (url))
+quit()
 
 choices = (
 	('Audio', 'bestaudio[ext=m4a]', 'm4a'),
 	('Video', 'best[ext=mp4]', 'mp4'),
 )
-choice = console.alert('youtube-dl', 'Version to extract:',
-	*(c[0] for c in choices))
+#choice = console.alert('youtube-dl', 'Version to extract:',
+#	*(c[0] for c in choices))
+
+#choice = choices[0] if dl_type == "audio"
+choice = None
 
 _, format, ext = choices[choice-1]
 print('format: %s' % (format))
